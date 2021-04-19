5d7df5a0d6184de1032f51d56da1fa0643ead3e5
Alireza-Mahinparvar <42847715+Alireza-Mahinparvar@users.noreply.github.com>
2021-04-19 10:45:20 -0700

    Update handlers_general.c

--- projects/lpc40xx_freertos/l4_io/cli/handlers/handlers_general.c
+++ projects/lpc40xx_freertos/l4_io/cli/handlers/handlers_general.c
@@ -84,6 +84,17 @@ app_cli_status_e cli__task_control(app_cli__argument_t argument,
   return APP_CLI_STATUS__SUCCESS;
 }
 
+app_cli_status_e cli__mp3_play(app_cli__argument_t argument,
+                               sl_string_t user_input_minus_command_name,
+                               app_cli__print_string_function cli_output) {
+  // user_input_minus_command_name is actually a 'char *' pointer type
+  // We tell the Queue to copy 32 bytes of songname from this location
+  xQueueSend(Q_songname, user_input_minus_command_name, portMAX_DELAY);
+  
+  printf("Sent %s over to the Q_songname\n", user_input_minus_command_name);
+  return APP_CLI_STATUS__SUCCESS;
+}
+
 static void cli__task_list_print(sl_string_t output_string,
                                  app_cli__print_string_function cli_output) {
   void *unused_cli_param = NULL;
