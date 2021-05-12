8db7a3416f08b7b95e1e58b6b4e0027439468b4f
srivishnugv <68129508+srivishnugv@users.noreply.github.com>
2021-05-12 14:30:45 -0700

    Update LCD Driver (Added all menu UI)

--- projects/lpc40xx_freertos/l5_application/2004_LCD.h
+++ projects/lpc40xx_freertos/l5_application/2004_LCD.h
@@ -15,3 +15,13 @@ void LCD2004_print(int rows, int cols, char string[20]);
 void LCD2004_clear(void);
 
 void LCD2004_menu1(char name[20]);
+
+void LCD2004_menu_play(char name[20]);
+
+void LCD2004_menu_volume(char name[20]);
+
+void LCD2004_menu_treb_bass(char name[20]);
+
+void LCD2004_menu_treble(char name[20]);
+
+void LCD2004_menu_bass(char name[20]);
